61b4ffa6f43d29b9c61a9f2d159ef74114d10f91
L00133105 <45152435+L00133105@users.noreply.github.com>
2020-02-24 19:11:03 +0000

    ScrapeAxios

---
+++ ScrapeAxios.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('cnn.csv');
+const SCRAPING_URL = 'https://edition.cnn.com/';
+
+
+(async () => {
+  const response = await axios.get(SCRAPING_URL)
+    .then(res => res.data)
+    .catch(err => console.log(err));
+
+  const results = [];
+
+  if (response) {
+    const $ = cheerio.load(response);
+
+    $('span.ob-unit ob-rec-image-container').each(function() {
+      results.push($(this).text());
+      // Write Row To CSV
+      writeStream.write(`${results} /n`);
+    });
+  }
+  console.log(results);
+})();
