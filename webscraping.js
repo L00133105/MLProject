b225ede5f06d46d9d887dc12c291599e43554dc8
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-03-01 04:00:24 +0000

    url scraping a

--- webscraping.js
+++ webscraping.js
@@ -1,12 +1,14 @@
 const request = require('request');
 const cheerio = require('cheerio');
-request('https://www.lyit.ie', (error, response, html) => {
+
+// https://www.techradar.com/ WORKS
+request('', (error, response, html) => {
     if(!error && response.statusCode==200){
         const $ = cheerio.load(html);
         const siteHeading = $('.site-heading');
         console.log(siteHeading.html());
 
-        $('.nav-item a').each((i, el) => {
+        $('link').each((i, el) => {
             const item = $(el).text();
             const link = $(el).attr('href');
             console.log(link);
