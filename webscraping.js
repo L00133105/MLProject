7ed02cdd37f51a1cbd870b86526b3a6db9be7502
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-03-01 04:12:51 +0000

    link works, trying to use both 'a' && 'link'

--- webscraping.js
+++ webscraping.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const cheerio = require('cheerio');
 // Use Loop to cycle through .txt file for each URL :)
-var url = 'https://www.techradar.com/';
+var url = 'https://www.edition.cnn.com/';
 
 request(url, (error, response, html) => {
     if(!error && response.statusCode==200){
