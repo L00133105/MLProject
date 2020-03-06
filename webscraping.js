95b0248e30fd5560151935f750bc7ecf70c15b02
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-03-06 03:33:31 +0000

    working on iterations

--- webscraping.js
+++ webscraping.js
@@ -2,21 +2,28 @@ const request = require('request');
 const cheerio = require('cheerio');
 var fs = require('fs');
 // Use Loop to cycle through .txt file for each URL :)
-//var url = 'urls.txt';
-var url = 'https://techradar.com/';
+ var url = "http://techradar.com";
+var urls = 'urls.txt';
+var urlList =[];
+
 request(url, (error, response, html) => {
     if(!error && response.statusCode==200){
         const $ = cheerio.load(html);
-        const siteHeading = $('.site-heading');
-        console.log(siteHeading.html());
-
+        // for(var e=0; e<urls.length; e++){
+        //     urlList.push(urls[e].href);
+        //     $('link').each((i, el) => {
+        //         const item = $(el).text();
+        //         const link = $(el).attr('href');
+        //         console.log(link);
+        //         fs.appendFileSync('websiteurls.txt', " " + link + '\n');
+        //     });
+        // }
+        // console.log(urlList);
         $('link').each((i, el) => {
             const item = $(el).text();
             const link = $(el).attr('href');
             console.log(link);
-            // writeStream.write(`${link} /n`);
-           // writeStream.write('/n' + link + '/n');
-            fs.appendFileSync('websiteurls.txt', " " + link + '\n');
+            fs.appendFileSync('test.csv', " " + link + '\n');
         });
     }
 });
