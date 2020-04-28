7a51f63d2c16264e1a7e4698bd07e374fc7098de
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-04-28 20:44:51 +0100

    filtering urls to remove https

---
+++ removehttps.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var array = fs.readFileSync('techradar.txt').toString().split("\n");
+var urlextend1 = 'https://';
+var urlextend2 = 'http://';
+var wstream = fs.createWriteStream('httpsadded.txt'); 
+var substr = 'https://www.techradar.com/';
+for(i in array){
+     if(array[i].includes(urlextend1) > -1 && array[i].includes(urlextend2) > -1){
+         //console.log(array[i]);
+         var update = array[i].replace("https://www.", "");
+         var update1 = update.replace("http://www.", "");
+         var update2 = update1.replace("https://", "");
+         var update3 = update2.replace("http://", "");
+         var update4 = update3.replace("//", "");
+         //console.log(update4);
+         //Filtering URLs to only include Domains: to links
+    }
+    wstream.write(update4 + '\n');
+}
+
--- scrapehtml.js
+++ scrapehtml.js
@@ -1,26 +1,50 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const writeStream = fs.createWriteStream('cnn.csv');
-const SCRAPING_URL = 'https://techradar.com/';
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+// const fs = require('fs');
+// const writeStream = fs.createWriteStream('cnn.csv');
+// const SCRAPING_URL = 'https://paddypower.com/';
 
-(async () => 
+// (async () => 
+// {
+//   const response = await axios.get(SCRAPING_URL)
+//     .then(res => res.data)
+//     .catch(err => console.log(err));
+//   const results = [];
+//   if (response) 
+//   {
+//     const $ = cheerio.load(response);
+//     $('html, e').each(function() 
+//     {
+//       results.push($(this).text());
+//       //writeStream.write(results);
+//     });
+//   }
+//   console.log(results);
+// })();
+
+const request = require('request');
+const cheerio = require('cheerio');
+var _ = require('lodash');
+var fs = require('fs');
+var array = fs.readFileSync('URLS.txt').toString().split("\n");    
+var wstream = fs.createWriteStream('test.txt');
+for(i in array) 
 {
-  const response = await axios.get(SCRAPING_URL)
-    .then(res => res.data)
-    .catch(err => console.log(err));
-  const results = [];
-  if (response) 
-  {
-    const $ = cheerio.load(response);
-    $('html').each(function() 
+    var url = array[i];
+    request(url, (error, response, html) => 
     {
-      results.push($(this).text());
-      //writeStream.write(results);
+        if(!error && response.statusCode==200)
+        {
+            const $ = cheerio.load(html);
+            $('html, e').each((i, el) => 
+            {
+                var link = $(el).attr('html');
+                //wstream.write(link + '\n');
+                console.log(link);
+            });
+        }
     });
-  }
-  console.log(results);
-})();
+}
 
 // const request = require('request');
 // const cheerio = require('cheerio');
