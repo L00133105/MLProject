34eaa8c199328666d782ae4f4febc0ea413fe465
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-05-04 00:31:40 +0100

    automate

--- weekly.sh
+++ weekly.sh
@@ -1,6 +1,10 @@
 #!/bin/node
 cd /home/pi/Documents/MLProject/
 node urlscraping
+echo 'Scrape URLs'
 node removebasic
+echo 'Remove duplicate URLs'
 node remove https
+echo 'Remove and filter URLs'
 node scrapingCategorise
+echo 'Scrape and Categorise URLs'
