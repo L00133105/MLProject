26f72156b8efe82c8524c0f0cacd7caadabe6644
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-05-04 00:30:51 +0100

    automate

--- monthly.sh
+++ monthly.sh
@@ -1,10 +1,18 @@
 #!/bin/node
 cd /home/pi/Documents/MLProject/
 node keywordsAllScrape
+echo 'keyWordsScraped'
 node keywordsEntertainment
+echo 'Scrape and Count Entertainment Keywords'
 node keywordsExplicit
+echo 'Scrape and Count Explicit Keywords'
 node keywordsGambling
+echo 'Scrape and Count Gambling Keywords'
 node keywordsMarketingPlatform
+echo 'Scrape and Count Marketing Platform Keywords'
 node keywordsSport
+echo 'Scrape and Count Sport Keywords'
 node keywordsTechnology
+echo 'Scrape and Count Technology Keywords'
 node keywordsTravel
+echo 'Scrape and Count Travel Keywords'
