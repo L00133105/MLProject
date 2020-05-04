522f89dcfc638791b68830eec55867b8f2eeef5e
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-05-04 01:14:57 +0100

    automationDone

--- weekly.sh
+++ weekly.sh
@@ -4,6 +4,6 @@ echo "Scrape URLs"
 node removeDuplicates
 echo "Remove duplicate URLs"
 node removeExtension
-echo "Remove extnesions and filter URLs"
+echo "Remove extensions and filter URLs"
 node scrapingCategorise
 echo "Scrape and Categorise URLs"
