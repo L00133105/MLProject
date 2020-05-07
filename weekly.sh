17ee2ec925d53c3284c6aa602d974e1f87ab6326
Joseph Mc Laughlin <L00133105@student.lyit.ie>
2020-05-07 15:41:06 +0100

    updatedAutomation

--- weekly.sh
+++ weekly.sh
@@ -7,6 +7,7 @@ node removeExtension
 echo "Remove extensions and filter URLs"
 node scrapingCategorise
 echo "Scrape and Categorise URLs"
+git add -u
 git commit -m 'Weekly Script'
 git push
 echo "Pushed to GitHub"
